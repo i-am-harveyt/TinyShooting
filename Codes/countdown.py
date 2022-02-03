9f93c0d2dfdb2aa545a8c0b4feefb74ee122a716
Hao-Ting Tung <haotingtong30@gmail.com>
2022-02-03 14:56:50 +0800

    Adjust coding style

--- Codes/countdown.py
+++ Codes/countdown.py
@@ -3,17 +3,16 @@ import pygame
 
 def countDown(SCREEN):
     countDownCLOCK = pygame.time.Clock()
-    images = [
-        pygame.image.load("Graphics/Countdown3.png").convert(),
-        pygame.image.load("Graphics/Countdown2.png").convert(),
-        pygame.image.load("Graphics/Countdown1.png").convert()
-    ]
-    currentImage = 0
+    currentImage = 3
     while True:
-        if currentImage >= 3:
+        if currentImage <= 0:
             break
-        SCREEN.blit(images[currentImage], (0, 0))
+        SCREEN.blit(
+            pygame.image.load(
+                f"Graphics/Countdown{currentImage}.png").convert(), 
+                (0, 0)
+                )
         pygame.display.flip()
-        currentImage += 1
+        currentImage -= 1
 
         countDownCLOCK.tick(1)
